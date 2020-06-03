938c6b087bebb513b6ff7e20101fb67133519910
krnchittora2 <65993497+krnchittora2@users.noreply.github.com>
2020-06-03 18:36:40 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function change(id){
+	document.getElementById("iitb").className = "";
+	document.getElementById("school").className = "";
+	document.getElementById("places").className = "";
+	document.getElementById("comm").className = "";
+	document.getElementById(id).className = "active";
+}
