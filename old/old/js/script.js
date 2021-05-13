751b055d9f3b850623abd2d3a7a6fb70ae727857
krnchittora2 <65993497+krnchittora2@users.noreply.github.com>
2021-05-13 11:44:12 +0530

    Removed blogs

--- old/old/js/script.js
+++
@@ -1,7 +0,0 @@
-function change(id){
-	document.getElementById("iitb").className = "";
-	document.getElementById("school").className = "";
-	document.getElementById("places").className = "";
-	document.getElementById("comm").className = "";
-	document.getElementById(id).className = "active";
-}
